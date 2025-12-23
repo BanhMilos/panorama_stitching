c6ed51e500b00ab6964d9211b34092d45a97721f
BanhMilos <hungann426@gmail.com>
2025-12-23 14:43:53 +0700

    finish, support heic now, fkin opencv op

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import cv2
-image_paths=['1.jpg','2.jpg','3.jpg']
-# initialized a list of images
-imgs = []
+import numpy as np
+import glob
+import img_convert
+import sys
+import json
+import os
 
-for i in range(len(image_paths)):
-    imgs.append(cv2.imread(image_paths[i]))
-    imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.4,fy=0.4)
-    # this is optional if your input images isn't too large
-    # you don't need to scale down the image
-    # in my case the input images are of dimensions 3000x1200
-    # and due to this the resultant image won't fit the screen
-    # scaling down the images 
-# showing the original pictures
-cv2.imshow('1',imgs[0])
-cv2.imshow('2',imgs[1])
-cv2.imshow('3',imgs[2])
+def main():
+    image_unconverted_paths = glob.glob("unconverted/*.HEIC")
+    imgs = []
 
-stitchy=cv2.Stitcher.create()
-(dummy,output)=stitchy.stitch(imgs)
+    if (len(image_unconverted_paths) < 2):
+        json.dumps({'result' : "failed", "reason":"Need at least two images to stitch a panorama."})
+        sys.exit(1)
+    imgsByte = img_convert.convert_image_list_to_jpg_bytes(image_unconverted_paths,quality=95)
 
-if dummy != cv2.STITCHER_OK:
-  # checking if the stitching procedure is successful
-  # .stitch() function returns a true value if stitching is 
-  # done successfully
-    print("stitching ain't successful")
-else: 
-    print('Your Panorama is ready!!!')
+    for i in range(len(imgsByte)):
+        img = cv2.imdecode(np.frombuffer(imgsByte[i], np.uint8), cv2.IMREAD_COLOR)
+        if (img is None):
+            json.dumps({'result' : "failed", "reason":f"Image at {image_unconverted_paths[i]} could not be read."})
+            sys.exit(1)
+        imgs.append(img)
 
-# final output
-cv2.imshow('final result',output)
+    stitcer=cv2.Stitcher.create()
+    (status,output)=stitcer.stitch(imgs)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    if status != cv2.Stitcher_OK:
+        json.dumps({'result' : "failed", "reason": int(status)})
+        sys.exit(1)
+
+    output_path = "output/panorama.jpg"
+    mkdir_cmd = "mkdir -p output"
+    os.system(mkdir_cmd)
+    cv2.imwrite(output_path,output)
+    json.dumps({'result' : "success", "output_path": output_path})
+    sys.exit(0)    
+    
+if __name__ == "__main__":
+    main()
